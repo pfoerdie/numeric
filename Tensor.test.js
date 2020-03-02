d909fa78aca520852e1312675ad13728509e4223
Simon Petrac <pfoerdie@gmail.com>
2020-03-02 03:24:46 +0100

    wip

--- Tensor.test.js
+++ Tensor.test.js
@@ -5,15 +5,15 @@ const B = new Tensor(3, 2);
 const v = new Tensor(3);
 const w = new Tensor(3);
 
-console.log("A: " + A);
-console.log("B: " + B);
-console.log("v: " + v);
-console.log("w: " + w);
-
 B[0] = 1; B[1] = 2; B[5] = 3;
 A[0] = 1; A[4] = 1; A[8] = 1;
 v[0] = 1; v[1] = 2; v[2] = 3;
 w[0] = Math.PI; w[1] = Math.E; w[2] = Number.EPSILON;
+
+console.log("A: " + A);
+console.log("B: " + B);
+console.log("v: " + v);
+console.log("w: " + w);
 
 console.log("A*v: " + A.multiply(v));
 console.log("v.w: " + v.hadMultiply(w));

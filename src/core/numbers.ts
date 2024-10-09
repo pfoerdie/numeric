ce28525c8a1503e6bd2811a18df77e670d121f4b
Simon Petrac <spetrac@nicos-ag.com>
2024-10-09 13:45:36 +0200

    changed numbers constructor

--- src/core/numbers.ts
+++ src/core/numbers.ts
@@ -11,7 +11,7 @@ export function assertFloat(value: unknown): asserts value is float {
 
 export type float64 = float
 export function float64(value: number | string): float64 {
-  return float(value)
+  return Float64Array.of(float(value))[0]
 }
 export function isFloat64(value: unknown): value is float64 {
   return isFloat(value)
@@ -22,7 +22,7 @@ export function assertFloat64(value: unknown): asserts value is float64 {
 
 export type float32 = float64
 export function float32(value: number | string): float32 {
-  return Math.fround(float(value))
+  return Float32Array.of(float(value))[0]
 }
 export function isFloat32(value: unknown): value is float32 {
   return isFloat(value) && (Number.isNaN(value) || Math.fround(value) === value)
@@ -44,7 +44,7 @@ export function assertInt(value: unknown): asserts value is int {
 
 export type int32 = int
 export function int32(value: number | string): int32 {
-  return Math.min(Math.max(int(value), -2147483648), 2147483647)
+  return Int32Array.of(int(value))[0]
 }
 export function isInt32(value: unknown): value is int32 {
   return isInt(value) && value >= -2147483648 && value <= 2147483647
@@ -55,7 +55,7 @@ export function assertInt32(value: unknown): asserts value is int32 {
 
 export type int16 = int32
 export function int16(value: number | string): int16 {
-  return Math.min(Math.max(int(value), -32768), 32767)
+  return Int16Array.of(int(value))[0]
 }
 export function isInt16(value: unknown): value is int16 {
   return isInt(value) && value >= -32768 && value <= 32767
@@ -66,7 +66,7 @@ export function assertInt16(value: unknown): asserts value is int16 {
 
 export type int8 = int16
 export function int8(value: number | string): int8 {
-  return Math.min(Math.max(int(value), -128), 127)
+  return Int8Array.of(int(value))[0]
 }
 export function isInt8(value: unknown): value is int8 {
   return isInt(value) && value >= -128 && value <= 127
@@ -77,7 +77,7 @@ export function assertInt8(value: unknown): asserts value is int8 {
 
 export type uint = int
 export function uint(value: number | string): uint {
-  return Math.max(int(value), 0)
+  return Math.abs(int(value))
 }
 export function isUint(value: unknown): value is uint {
   return isInt(value) && value >= 0
@@ -88,7 +88,7 @@ export function assertUint(value: unknown): asserts value is uint {
 
 export type uint32 = uint
 export function uint32(value: number | string): uint32 {
-  return Math.min(uint(value), 4294967295)
+  return Uint32Array.of(int(value))[0]
 }
 export function isUint32(value: unknown): value is uint32 {
   return isUint(value) && value <= 4294967295
@@ -99,7 +99,7 @@ export function assertUint32(value: unknown): asserts value is uint32 {
 
 export type uint16 = uint32
 export function uint16(value: number | string): uint16 {
-  return Math.min(uint(value), 65535)
+  return Uint16Array.of(int(value))[0]
 }
 export function isUint16(value: unknown): value is uint16 {
   return isUint(value) && value <= 65535
@@ -110,7 +110,7 @@ export function assertUint16(value: unknown): asserts value is uint16 {
 
 export type uint8 = uint16
 export function uint8(value: number | string): uint8 {
-  return Math.min(uint(value), 255)
+  return Uint8Array.of(int(value))[0]
 }
 export function isUint8(value: unknown): value is uint8 {
   return isUint(value) && value <= 255
